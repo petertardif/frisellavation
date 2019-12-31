da1d754f66492eeba034a74c830080dc8f75669c
Peter Tardif <peter.tardif@gmail.com>
2019-12-30 17:45:47 -0700

    adding in data fetching with apollo and hooks

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { AppLoading } from 'expo';
 import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
-import { Audio } from "expo-av";
 import React, { useState } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-
 import AppNavigator from './navigation/AppNavigator';
 
 export default function App(props) {
+  const client = new ApolloClient({
+    uri: 'https://us1.prisma.sh/peter-tardif-0f28bc/frisellavation_server/dev',
+  });
   const [isLoadingComplete, setLoadingComplete] = useState(false);
 
   if (!isLoadingComplete && !props.skipLoadingScreen) {
@@ -22,8 +26,10 @@ export default function App(props) {
   } else {
     return (
       <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
+        <ApolloProvider client={client}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          <AppNavigator />
+        </ApolloProvider>
       </View>
     );
   }
